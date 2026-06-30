5d338fcb9b25a23cccdd523e67c57dfc4a07d7c4
abiranjitham23-wq <abiranjitham23@gmail.com>
2026-06-30 12:32:30 +0530

    Rename TempCard to Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Card from "./components/Card";
+import Card from "./components/TempCard";
 import cardImages from "./data";
 
 function App() {
